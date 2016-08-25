8899fd74a6fd5c5005b6e8fb176a166ade26fe85
May Miller-Ricci <maymillerricci@gmail.com>
2016-08-24 21:53:24 -0700

    add comments planning out tic-toe-toe play

---
+++ js/app.js
@@ -0,0 +1,27 @@
+"use strict"
+
+// * use module pattern
+
+// on page load, show start.txt markup
+// add to html, hide the rest
+
+// on click start
+// hide start, show board
+
+// o goes first
+// maybe: next game winner goes first?
+// o is highlighted
+// hover is o shaped over spaces
+// on click square, if empty, square fills with solid o
+// then x's turn...
+// method for playing a turn, pass in x/o
+
+// alternate playing turns until 3 in a row, or board filled
+
+// at the end of each turn, check for 3 in a row
+// if winner, end game
+// else next turn
+
+// if winner, end game = o winner view or x winner view
+// if tie, tie view
+// on click new game button, starts it all over again
