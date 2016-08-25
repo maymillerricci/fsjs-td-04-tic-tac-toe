36e640ddfcf1d8c1131a659c839b02668ed2887e
May Miller-Ricci <maymillerricci@gmail.com>
2016-08-24 22:18:35 -0700

    on click start game button

--- js/app.js
+++ js/app.js
@@ -1,24 +1,29 @@
 "use strict"
 
-// * use module pattern
+var ticTacToe = (function() {
+  // on click start button, hide start page and show board
+  $("#start-game").on("click", function(e) {
+    e.preventDefault();
+    $("#start").addClass("hidden");
+    $("#board").removeClass("hidden");
+  });
 
-// on click start
-// hide start, show board
+  // o goes first
+  // maybe: next game winner goes first?
+  // o is highlighted
+  // hover is o shaped over spaces
+  // on click square, if empty, square fills with solid o
+  // then x's turn...
+  // method for playing a turn, pass in x/o
 
-// o goes first
-// maybe: next game winner goes first?
-// o is highlighted
-// hover is o shaped over spaces
-// on click square, if empty, square fills with solid o
-// then x's turn...
-// method for playing a turn, pass in x/o
+  // alternate playing turns until 3 in a row, or board filled
 
-// alternate playing turns until 3 in a row, or board filled
+  // at the end of each turn, check for 3 in a row
+  // if winner, end game
+  // else next turn
 
-// at the end of each turn, check for 3 in a row
-// if winner, end game
-// else next turn
+  // if winner, end game = o winner view or x winner view
+  // if tie, tie view
+  // on click new game button, starts it all over again
 
-// if winner, end game = o winner view or x winner view
-// if tie, tie view
-// on click new game button, starts it all over again
+}());
